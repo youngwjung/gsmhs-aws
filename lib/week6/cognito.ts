8c5aa68ff0ba087d1f94b412c067315bc8b1d252
Young Woo Jung <youngwjung@gmail.com>
2021-02-19 15:48:16 +0900

    update cognito lab

--- lib/week6/cognito.ts
+++ lib/week6/cognito.ts
@@ -1,4 +1,5 @@
 import * as cdk from "@aws-cdk/core";
+import * as ec2 from "@aws-cdk/aws-ec2";
 import * as cognito from "@aws-cdk/aws-cognito";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as iam from "@aws-cdk/aws-iam";
@@ -7,6 +8,35 @@ import { CfnOutput } from "@aws-cdk/core";
 export class CognitoStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, "vpc", {
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          name: "public",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
+    });
+
+    const user_data = ec2.UserData.forLinux();
+    user_data.addCommands("echo cognito > /home/ec2-user/sample.txt");
+
+    const instance = new ec2.Instance(this, "instance", {
+      vpc: vpc,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T3,
+        ec2.InstanceSize.MICRO
+      ),
+      machineImage: ec2.MachineImage.latestAmazonLinux({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+      userData: user_data,
+    });
+
+    instance.role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AmazonEC2RoleforSSM")
+    );
 
     const user_pool = new cognito.UserPool(this, "user_pool");
 
